dad221b9f0dfd31e31f4967fb76d7d196a2293fa
vikash14_12@hotmail.com <vikash14_12@hotmail.com>
2021-12-29 13:45:19 +0000

    update

--- Notebooks/TestNotebook.py
+++ Notebooks/TestNotebook.py
@@ -15,25 +15,8 @@
 
 #create csv of testresult table and save to blob storage
 # Azure Storage Account Name
-storage_account_name = "globalproducts"
-
-# Azure Storage Account Key
-storage_account_key = "onoPvW5rPcUoa2AoJnTzv4UXijmIC6KU8pXIbMYXq+vxkX2T7VOpb1nrstBz3Vw4FN/e8W5EoEaCqwqhsFoCxA=="
-
-# Azure Storage Account Source Container
-container = "globalproducts"
-
-# Set the configuration details to read/write
-spark.conf.set("fs.azure.account.key.{0}.blob.core.windows.net".format(storage_account_name), storage_account_key)
+#https://medium.com/@zifah/how-to-write-data-from-an-azure-databricks-notebook-to-an-azure-blob-storage-container-283bfdf23893
 
 # COMMAND ----------
 
-# MAGIC %scala
-# MAGIC 
-# MAGIC val df_spark= spark.table("default.testresult")
-# MAGIC 
-# MAGIC 
-# MAGIC spark.conf.set("fs.azure.account.key.azurestorage.blob.core.windows.net","onoPvW5rPcUoa2AoJnTzv4UXijmIC6KU8pXIbMYXq+vxkX2T7VOpb1nrstBz3Vw4FN/e8W5EoEaCqwqhsFoCxA==")
-# MAGIC 
-# MAGIC // Save to the source container
-# MAGIC df_spark.write.mode(SaveMode.Append).csv("wasbs://globalproducts@globalproducts.blob.core.windows.net/TestResult/")
+
